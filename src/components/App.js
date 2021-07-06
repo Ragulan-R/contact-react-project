7e945017aa901a969b5ab880408dda0263aa21ab
ragulan <ragulanravi21@gmail.com>
2021-07-06 14:49:35 -0400

    added contact form functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Header from './Header'
 import AddContact from './AddContact'
 import ContactList from './ContactList'
 
 function App() {
-  const contacts = [
-    {
-      id: '1',
-      name: 'Ragulan',
-      email: 'ragulancodes@gmail.com',
-    },
-    {
-      id: '2',
-      name: 'Ricky',
-      email: 'rickycodes@gmail.com',
-    },
-  ]
+  const [contacts, setContacts] = useState([])
+  // intially will be an empty array
+
+  // const contacts = [
+  //   {
+  //     id: '1',
+  //     name: 'Ragulan',
+  //     email: 'ragulancodes@gmail.com',
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'Ricky',
+  //     email: 'rickycodes@gmail.com',
+  //   },
+  // ]
 
   return (
     <div className='ui container'>
