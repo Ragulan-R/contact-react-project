52184a7ef9635def438aabada455e477e732227f
ragulan <ragulanravi21@gmail.com>
2021-07-06 09:36:36 -0400

    added imports and returns in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+
+import React from 'react'
+import './App.css'
+import Header from './Header'
+import AddContact from './AddContact'
+import ContactList from './ContactList'
 
 function App() {
   return (
-  <div>Hello World</div>
-  );
-}
+    <div>
+      <Header/>
+      <AddContact/>
+      <ContactList/>
+    </div>
+  )
 
-export default App;
+export default App
