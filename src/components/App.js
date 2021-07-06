af62f24dc301b238105d0580fbdb8612d77bbb52
ragulan <ragulanravi21@gmail.com>
2021-07-06 09:58:48 -0400

    added form in addcontact component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import './App.css'
 import Header from './Header'
@@ -8,10 +7,11 @@ import ContactList from './ContactList'
 function App() {
   return (
     <div>
-      <Header/>
-      <AddContact/>
-      <ContactList/>
+      <Header />
+      <AddContact />
+      {/* <ContactList /> */}
     </div>
   )
+}
 
 export default App
