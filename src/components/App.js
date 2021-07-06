bfa343e2c360396ca03a6b4427e90faef6ff46ab
ragulan <ragulanravi21@gmail.com>
2021-07-06 11:40:07 -0400

    set up map and render of contact list

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,25 @@ import AddContact from './AddContact'
 import ContactList from './ContactList'
 
 function App() {
+  const contacts = [
+    {
+      id: '1',
+      name: 'Ragulan',
+      email: 'ragulancodes@gmail.com',
+    },
+    {
+      id: '2',
+      name: 'Ricky',
+      email: 'rickycodes@gmail.com',
+    },
+  ]
+
   return (
     <div className='ui container'>
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
+      {/* passing contacts array to contact component */}
     </div>
   )
 }
